6b7d782234dd08eee1753d66bfc4bd6cab1b4cc6
PivotAnteon <rylan@trackd.run>
2025-02-09 13:35:06 -0600

    Problems with computer, original push

--- openf1.js
+++ openf1.js
@@ -1,5 +1,8 @@
+var userInput;
 function getDriverInfo(){
-    fetch('https://api.openf1.org/v1/drivers?driver_number=1&session_key=9158')
+    userInput = document.getElementById("driverNumber").value;
+    fetch(`https://api.openf1.org/v1/drivers?driver_number=${userInput}&session_key=9158`)
     .then(response => response.json())
-    .then(jsonContent => console.log(jsonContent));
+    .then(jsonContent => console.log(jsonContent))
+    .then(data => {console.log(data.full_name)});
 }
